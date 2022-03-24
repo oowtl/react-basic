b49503ccb8a1ea81c6ec6a484e0ffe850579eecf
oowtl <oowtl7425@gmail.com>
2022-03-24 21:37:30 +0900

    Updates

---
+++ precache-manifest.2110f47ea7e9b46601191144b6ce1dd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7355543cbcf19868edd45aa8c2d69a8b",
+    "url": "/react-basic/index.html"
+  },
+  {
+    "revision": "5a2f674442e60099066e",
+    "url": "/react-basic/static/css/main.8b73e418.chunk.css"
+  },
+  {
+    "revision": "37e74f651adef71b22cd",
+    "url": "/react-basic/static/js/2.e8f148ce.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/react-basic/static/js/2.e8f148ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a2f674442e60099066e",
+    "url": "/react-basic/static/js/main.dcaa4f01.chunk.js"
+  },
+  {
+    "revision": "3cfd1c96144847c17b2e",
+    "url": "/react-basic/static/js/runtime-main.84daba75.js"
+  }
+]);
